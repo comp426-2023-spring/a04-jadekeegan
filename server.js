d36d176776cc939c54b083f9347bb59013ca2b9f
jadekeegan <jadek@unc.edu>
2023-03-16 17:22:53 -0400

    Fix request

--- server.js
+++ server.js
@@ -23,26 +23,22 @@ app.get('/app/rpsls', (req, res) => {
 	res.status(200).send(rpsls());
 })
 
-app.get('/app/rps/play', (req, res) => {
-	res.status(200).send(rps(req.query.shot));
+app.post('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.body.shot));
 })
 
-app.get('/app/rpsls/play', (req, res) => {
-	res.status(200).send(rpsls(req.query.shot));
+app.post('/app/rpsls/play', (req, res) => {
+	res.status(200).send(rpsls(req.body.shot));
 })
 
-app.post('/app/rps/play/:shot', (req, res) => {
+app.get('/app/rps/play/:shot', (req, res) => {
 	res.status(200).send(rps(req.params.shot));
 })
 
-app.post('/app/rps/play/:shot', (req, res) => {
+app.get('/app/rps/play/:shot', (req, res) => {
 	res.status(200).send(rpsls(req.params.shot));
 })
 
 app.get('*', (req, res) => {
 	res.status(404).send('404 NOT FOUND');
-})
-
-app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`);
 })
