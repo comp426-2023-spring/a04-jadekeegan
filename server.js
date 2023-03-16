39eec8c5b371ed65ce9bd0a6f20d755dc7249455
jadekeegan <jadek@unc.edu>
2023-03-16 17:02:56 -0400

    Add json/url-encoded support

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 var args = minimist(process.argv.slice(2));
 const PORT = args.port || 5000
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.get('/app/', (req, res) => {
 	res.status(200).send("200 OK")
 });
