28b4db0a808a6d77d99e0b322297732fcd7fe3de
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-03-16 16:49:04 -0400

    Fix syntax error (missing parantheses)

--- server.js
+++ server.js
@@ -17,31 +17,31 @@ app.listen(PORT, (error) => {
 
 app.get('/app/', (req, res) => {
 	res.status(200).send("200 OK")
-}
+});
 
 app.get('/app/rps/', (req, res) => {
 	res.status(200).send(rps());
-}
+});
 
 app.get('/app/rpsls/', (req, res) => {
 	res.status(200).send(rpsls())
-}
+});
 
 app.get('/app/rps/play', (req, res) => {
 	res.status(200).send(rps(req.body))
-}
+});
 
 app.get('/app/rpsls/play/', (req, res) => {
 	res.status(200).send(rpsls(req.body))
-}
+});
 
 app.get('/app/rps/play/(rock|paper|scissors)/', (req, res) => {
 	res.status(200).send(rps(req.params[0]))
-}
+});
 
 app.get('/app/rps/play/(rock|paper|scissors|lizard|spock)/', (req, res) => {
 	res.status(200).send(rpsls(req.params[0]))
-}
+});
 
 app.get('*', (req, res) => {
 	res.status(404).send('404 NOT FOUND');
